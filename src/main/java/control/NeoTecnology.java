839ab33cabe16ddc40511df517c0953f29c9128f
NataliaHv <imaginacion05@hotmail.com>
2020-10-15 23:23:14 -0500

    mi primer commit

---
+++ src/main/java/control/NeoTecnology.java
@@ -0,0 +1,173 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Updates.combine;
+import static com.mongodb.client.model.Updates.set;
+import com.mongodb.client.result.DeleteResult;
+import java.util.Arrays;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author nataliahv
+ */
+public class NeoTecnology {
+
+    public String consultarClientes() {
+        //Creacion del cliente para conectar al servidor remoto
+        MongoClient cliente;
+
+        //la uri es una ruta para conectarse en este caso al servidor remoto
+        MongoClientURI uri = new MongoClientURI("mongodb://userLab6:passworduserLab6@93.188.167.110:27017/?authSource=lab6");
+        cliente = new MongoClient(uri);
+        //se crea el objeto de type MongoDatabase para conectarse y obtener la informacion de la base de datos que se encuentra en el servidor
+        MongoDatabase db;
+        db = cliente.getDatabase("lab6");
+        //asi se obtiene la colecction y return el la coleccion de nosotros llamada clientes
+        MongoCollection<Document> collectionNeoTecnology = db.getCollection("clientes");
+        return collectionNeoTecnology.find().first().toJson();
+
+    }
+
+    //public String eliminarUnDocumento(String id) {
+     //   MongoClient cliente;
+     //   MongoClientURI uri = new MongoClientURI("mongodb://userLab6:passworduserLab6@93.188.167.110:27017/?authSource=lab6");
+     //   cliente = new MongoClient(uri);
+
+       // MongoDatabase db;
+      //  db = cliente.getDatabase("lab6");
+     //   MongoCollection<Document> collectionNeoTecnology = db.getCollection("clientes");
+
+       // DeleteResult result = collectionNeoTecnology.deleteOne((Bson) collectionNeoTecnology.deleteOne(eq("_id", new ObjectId(id))));
+     //   return "(\"Confirmation\":\"" + result.getDeletedCount() + "\")";
+    //}
+        public String eliminarDocumento(String idEliminar){
+        
+        MongoClient mongoClient;
+        MongoClientURI uri = new MongoClientURI("mongodb://userLab6:passworduserLab6@93.188.167.110:27017/?authSource=lab6");
+        MongoDatabase db;
+        mongoClient = new MongoClient(uri);
+        db = mongoClient.getDatabase("lab6");
+        MongoCollection<Document> collection = db.getCollection("clientes");
+        collection.deleteOne(eq("_id", new ObjectId(""+idEliminar)));
+        
+      
+        return "{\"comfirmation\" : 1}";
+       
+        
+        
+}
+
+
+
+    public String consultarUltimoDocumento() {
+        //Creacion del cliente para conectar al servidor remoto
+        MongoClient empleado;
+
+        //la uri es una ruta para conectarse en este caso al servidor remoto
+        MongoClientURI uri = new MongoClientURI("mongodb://userLab6:passworduserLab6@93.188.167.110:27017/?authSource=lab6");
+        empleado = new MongoClient(uri);
+        //se crea el objeto de type MongoDatabase para conectarse y obtener la informacion de la base de datos que se encuentra en el servidor
+        MongoDatabase db;
+        db = empleado.getDatabase("lab6");
+        //asi se obtiene la ultima colecction y retorna el la coleccion de nosotros llamada empleados
+        MongoCollection<Document> collectionNeoTecnology = db.getCollection("empleados");
+        
+        FindIterable<Document> mySort = collectionNeoTecnology.find().sort(new BasicDBObject("_id",-1)).limit(1);
+        
+        MongoCursor<Document> cursor = mySort.iterator();
+        
+        while(cursor.hasNext()){
+            return cursor.next().toJson();
+        }
+
+       //return collectionNeoTecnology.find().sort({"_id" : -1}).limit(1).toJson();
+        //descending("quantity", "totalAmount") 
+        //return collectionNeoTecnology.find().sort(Sorts.descending({"_id", -1).limit(1).toJson();
+                //descending("codigo")).first().toJson();
+        //return collectionNeoTecnology.find().sort({"_id" : -1}).limit(1).toString();
+        return null;
+    }
+
+    
+    public String consultarCincoUltimos() {
+        //Creacion del cliente para conectar al servidor remoto
+        MongoClient cliente;
+
+        //la uri es una ruta para conectarse en este caso al servidor remoto
+        MongoClientURI uri = new MongoClientURI("mongodb://userLab6:passworduserLab6@93.188.167.110:27017/?authSource=lab6");
+        cliente = new MongoClient(uri);
+        //se crea el objeto de type MongoDatabase para conectarse y obtener la informacion de la base de datos que se encuentra en el servidor
+        MongoDatabase db;
+        db = cliente.getDatabase("lab6");
+        //asi se obtiene la ultima colecction y retorna el la coleccion de nosotros llamada empleados
+        MongoCollection<Document> collectionNeoTecnology = db.getCollection("clientes");
+        
+        FindIterable<Document> mySort = collectionNeoTecnology.find().sort(new BasicDBObject("_id",-1)).limit(4);
+        
+        MongoCursor<Document> cursor= mySort.iterator();
+        
+        String holi_String = "[";
+        while(cursor.hasNext()){
+            holi_String = holi_String.concat(cursor.next().toJson());
+            holi_String = holi_String.concat(",");
+            
+        }
+
+        holi_String = holi_String.concat("]");
+        return holi_String;
+        //Consumer<Document> printConsumer=new Consumer<Document>(){
+           // @Override
+           // public String accept (final Document document){
+               // System.out.print(document.toJson());
+               // return cursor.next().toJson();
+        }    
+        public String actualizarUnDocumento(String id, String nombre, String cédula) {
+        //Creacion del cliente para conectar al servidor remoto
+        MongoClient empleado;
+
+        //la uri es una ruta para conectarse en este caso al servidor remoto
+        MongoClientURI uri = new MongoClientURI("mongodb://userLab6:passworduserLab6@93.188.167.110:27017/?authSource=lab6");
+        empleado = new MongoClient(uri);
+        //se crea el objeto de type MongoDatabase para conectarse y obtener la informacion de la base de datos que se encuentra en el servidor
+        MongoDatabase db;
+        db = empleado.getDatabase("lab6");
+        //asi se obtiene la colecction y return el la coleccion de nosotros llamada clientes
+        MongoCollection<Document> collectionNeoTecnology = db.getCollection("empleados");
+        
+        collectionNeoTecnology.updateOne(eq("_id,",new ObjectId(id)),
+                  combine(set("nombre","Jose Victoria Peña"), set("cédula","1114567809")));
+        
+        
+        return "{\"Confirmation\":1}";
+
+    }   
+
+    
+              
+      
+    
+}
+        //return collectionNeoTecnology.find().sort({"_id" : -1}).limit(1).toJson();
+        //descending("quantity", "totalAmount") 
+        //return collectionNeoTecnology.find().sort(Sorts.descending({"_id", -1).limit(1).toJson();
+                //descending("codigo")).first().toJson();
+        //return collectionNeoTecnology.find().sort({"_id" : -1}).limit(1).toString();
+        //return collectionNeoTecnology.find().sort(Sorts.ascending("clientes")).first().toJson();
+        
+
+
---
+++ src/main/java/vista/Ws_neotecnology.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+
+import com.mongodb.client.FindIterable;
+import control.NeoTecnology;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import org.bson.Document;
+
+/**
+ * REST Web Service
+ *
+ * @author nataliahv
+ */
+@Path("ws_neotecnology")
+public class Ws_neotecnology {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of Ws_neotecnology
+     */
+    public Ws_neotecnology() {
+    }
+    
+    @GET
+    @Path("consultarClientes")
+    @Produces({"application/json"})
+    public String consultarClientes(){
+        
+        NeoTecnology miNeoTecnology = new NeoTecnology();
+        return miNeoTecnology.consultarClientes();
+    }
+    
+   // @GET
+  //  @Path("eliminarUnDocumento/id/{id}")
+ //   @Produces({"application/json"})
+ //   public void eliminarUnDocumento(@PathParam("id")String id){
+        
+  //      NeoTecnology miNeoTecnology = new NeoTecnology();
+  //      miNeoTecnology.eliminarUnDocumento(id);
+  ///  }
+    
+    @GET
+    @Path("eliminarDocumento/idEliminar/{idEliminar}")
+    @Produces({"application/json"})
+    public String eliminarDocumento(@PathParam("idEliminar") String idEliminar)  {
+        NeoTecnology miNeotecnology = new NeoTecnology();
+
+        return miNeotecnology.eliminarDocumento(idEliminar);
+        
+    }
+
+
+    
+    @GET
+    @Path("consultarUltimoDocumento")
+    @Produces({"application/json"})
+    public String consultarUltimoDocumento(){
+        
+        NeoTecnology miNeoTecnology = new NeoTecnology();
+        return miNeoTecnology.consultarUltimoDocumento();
+    }
+    @GET
+    @Path("consultarCincoUltimos")
+    @Produces({"application/json"})
+    public String consultarCincoUltimos(){
+        
+        NeoTecnology miNeoTecnology = new NeoTecnology();
+        return miNeoTecnology.consultarCincoUltimos();
+    }
+   
+  @GET
+  @Path("actualizarUnDocumento/id/{id}/nombre/{nombre}/cédula/{cédula}")
+  @Produces({"application/json"})
+  public void actualizarUnDocumento(@PathParam("id") String id, @PathParam("nombre") String nombre , @PathParam("cédula")String cédula){
+     NeoTecnology miNeoTecnology = new NeoTecnology();
+     miNeoTecnology.actualizarUnDocumento(id, nombre, cédula);  
+}
+}
