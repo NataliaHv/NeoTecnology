563c258231294539cf9c9302e37260e06f2004a3
NataliaHv <imaginacion05@hotmail.com>
2020-10-17 14:23:55 -0500

    Implementacion de excepciones en los servicios a y d,lo realizo NataliaH

--- src/main/java/control/NeoTecnology.java
+++ src/main/java/control/NeoTecnology.java
@@ -15,17 +15,18 @@ import com.mongodb.client.MongoDatabase;
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Updates.combine;
 import static com.mongodb.client.model.Updates.set;
-import com.mongodb.client.result.DeleteResult;
 import java.util.Arrays;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
+import javax.servlet.ServletException;
 
 /**
  *
  * @author nataliahv
  */
 public class NeoTecnology {
+    String respuesta = null;
 
     public String consultarClientes() {
         //Creacion del cliente para conectar al servidor remoto
@@ -39,40 +40,61 @@ public class NeoTecnology {
         db = cliente.getDatabase("lab6");
         //asi se obtiene la colecction y return el la coleccion de nosotros llamada clientes
         MongoCollection<Document> collectionNeoTecnology = db.getCollection("clientes");
-        return collectionNeoTecnology.find().first().toJson();
-
-    }
-
-    //public String eliminarUnDocumento(String id) {
-     //   MongoClient cliente;
-     //   MongoClientURI uri = new MongoClientURI("mongodb://userLab6:passworduserLab6@93.188.167.110:27017/?authSource=lab6");
-     //   cliente = new MongoClient(uri);
-
-       // MongoDatabase db;
-      //  db = cliente.getDatabase("lab6");
-     //   MongoCollection<Document> collectionNeoTecnology = db.getCollection("clientes");
-
-       // DeleteResult result = collectionNeoTecnology.deleteOne((Bson) collectionNeoTecnology.deleteOne(eq("_id", new ObjectId(id))));
-     //   return "(\"Confirmation\":\"" + result.getDeletedCount() + "\")";
-    //}
-        public String eliminarDocumento(String idEliminar){
         
+        try {
+          //  MongoCollection<Document> collection = db.getCollection("clientes");
+            respuesta += collectionNeoTecnology.find().first().toJson();
+            respuesta += "{\"confirmation\" : \"felicidades usted encontro el documento 1\"}";
+        } catch (Exception e) {
+            respuesta = "{\"cuidado\" : \" no encontro el documento \"}";
+
+        }
+        
+        return respuesta;
+        
+    
+       // return collectionNeoTecnology.find().first().toJson();
+
+        // try{
+        //  int []myNumbers={1}; 
+        //   System.out.println(myNumbers[2]);
+        //  }catch (Exception e){
+        //        System.out.println("{\"Cuidado \":\"1\"}"); 
+        //   }finally {
+    }
+    //public String eliminarUnDocumento(String id) {
+    //   MongoClient cliente;
+    //   MongoClientURI uri = new MongoClientURI("mongodb://userLab6:passworduserLab6@93.188.167.110:27017/?authSource=lab6");
+    //   cliente = new MongoClient(uri);
+
+    // MongoDatabase db;
+    //  db = cliente.getDatabase("lab6");
+    //   MongoCollection<Document> collectionNeoTecnology = db.getCollection("clientes");
+    // DeleteResult result = collectionNeoTecnology.deleteOne((Bson) collectionNeoTecnology.deleteOne(eq("_id", new ObjectId(id))));
+    //   return "(\"Confirmation\":\"" + result.getDeletedCount() + "\")";
+    //}
+    public String eliminarDocumento(String idEliminar) {
+
         MongoClient mongoClient;
         MongoClientURI uri = new MongoClientURI("mongodb://userLab6:passworduserLab6@93.188.167.110:27017/?authSource=lab6");
         MongoDatabase db;
         mongoClient = new MongoClient(uri);
         db = mongoClient.getDatabase("lab6");
-        MongoCollection<Document> collection = db.getCollection("clientes");
-        collection.deleteOne(eq("_id", new ObjectId(""+idEliminar)));
-        
-      
-        return "{\"comfirmation\" : 1}";
-       
-        
-        
-}
+        // MongoCollection<Document> collection = db.getCollection("clientes");
+        //  String respuesta = "";
+        //String respuesta = null;
+        try {
+            MongoCollection<Document> collection = db.getCollection("clientes");
+            collection.deleteOne(eq("_id", new ObjectId(idEliminar)));
+            respuesta = "{\"confirmation\" : \"felicidades usted pudo eliminar el documento\"}";
+        } catch (IllegalArgumentException e) {
+            respuesta = "{\"cuidado\" : \"el id no esta en la coleccion clientes\"}";
 
-
+        }
+        
+        return respuesta;
+        
+    }
 
     public String consultarUltimoDocumento() {
         //Creacion del cliente para conectar al servidor remoto
@@ -86,24 +108,23 @@ public class NeoTecnology {
         db = empleado.getDatabase("lab6");
         //asi se obtiene la ultima colecction y retorna el la coleccion de nosotros llamada empleados
         MongoCollection<Document> collectionNeoTecnology = db.getCollection("empleados");
-        
-        FindIterable<Document> mySort = collectionNeoTecnology.find().sort(new BasicDBObject("_id",-1)).limit(1);
-        
+
+        FindIterable<Document> mySort = collectionNeoTecnology.find().sort(new BasicDBObject("_id", -1)).limit(1);
+
         MongoCursor<Document> cursor = mySort.iterator();
-        
-        while(cursor.hasNext()){
+
+        while (cursor.hasNext()) {
             return cursor.next().toJson();
         }
 
-       //return collectionNeoTecnology.find().sort({"_id" : -1}).limit(1).toJson();
+        //return collectionNeoTecnology.find().sort({"_id" : -1}).limit(1).toJson();
         //descending("quantity", "totalAmount") 
         //return collectionNeoTecnology.find().sort(Sorts.descending({"_id", -1).limit(1).toJson();
-                //descending("codigo")).first().toJson();
+        //descending("codigo")).first().toJson();
         //return collectionNeoTecnology.find().sort({"_id" : -1}).limit(1).toString();
         return null;
     }
 
-    
     public String consultarCincoUltimos() {
         //Creacion del cliente para conectar al servidor remoto
         MongoClient cliente;
@@ -116,27 +137,28 @@ public class NeoTecnology {
         db = cliente.getDatabase("lab6");
         //asi se obtiene la ultima colecction y retorna el la coleccion de nosotros llamada empleados
         MongoCollection<Document> collectionNeoTecnology = db.getCollection("clientes");
-        
-        FindIterable<Document> mySort = collectionNeoTecnology.find().sort(new BasicDBObject("_id",-1)).limit(4);
-        
-        MongoCursor<Document> cursor= mySort.iterator();
-        
+
+        FindIterable<Document> mySort = collectionNeoTecnology.find().sort(new BasicDBObject("_id", -1)).limit(4);
+
+        MongoCursor<Document> cursor = mySort.iterator();
+
         String holi_String = "[";
-        while(cursor.hasNext()){
+        while (cursor.hasNext()) {
             holi_String = holi_String.concat(cursor.next().toJson());
             holi_String = holi_String.concat(",");
-            
+
         }
 
         holi_String = holi_String.concat("]");
         return holi_String;
         //Consumer<Document> printConsumer=new Consumer<Document>(){
-           // @Override
-           // public String accept (final Document document){
-               // System.out.print(document.toJson());
-               // return cursor.next().toJson();
-        }    
-        public String actualizarUnDocumento(String id, String nombre, String cédula) {
+        // @Override
+        // public String accept (final Document document){
+        // System.out.print(document.toJson());
+        // return cursor.next().toJson();
+    }
+
+    public String actualizarUnDocumento(String id, String nombre, String cédula) {
         //Creacion del cliente para conectar al servidor remoto
         MongoClient empleado;
 
@@ -148,26 +170,13 @@ public class NeoTecnology {
         db = empleado.getDatabase("lab6");
         //asi se obtiene la colecction y return el la coleccion de nosotros llamada clientes
         MongoCollection<Document> collectionNeoTecnology = db.getCollection("empleados");
-        
-        collectionNeoTecnology.updateOne(eq("_id,",new ObjectId(id)),
-                  combine(set("nombre","Jose Victoria Peña"), set("cédula","1114567809")));
-        
-        
-        return "{\"Confirmation\":1}";
 
-    }   
+        collectionNeoTecnology.updateOne(eq("_id,", new ObjectId(id)),
+                combine(set("nombre", "Jose Victoria Peña"), set("cédula", "1114567809")));
 
-    
-              
-      
-    
+        // return "{\"Confirmation\":1}";
+        return null;
+
+    }
+
 }
-        //return collectionNeoTecnology.find().sort({"_id" : -1}).limit(1).toJson();
-        //descending("quantity", "totalAmount") 
-        //return collectionNeoTecnology.find().sort(Sorts.descending({"_id", -1).limit(1).toJson();
-                //descending("codigo")).first().toJson();
-        //return collectionNeoTecnology.find().sort({"_id" : -1}).limit(1).toString();
-        //return collectionNeoTecnology.find().sort(Sorts.ascending("clientes")).first().toJson();
-        
-
-
